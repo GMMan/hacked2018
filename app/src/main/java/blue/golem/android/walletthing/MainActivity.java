69aec74a3edcc44197818c9c5342b1cc386f7613
Yukai Li <yukaili.geek@gmail.com>
2018-01-27 14:21:09 -0700

    Add UI elements for test converter

--- app/src/main/java/blue/golem/android/walletthing/MainActivity.java
+++ app/src/main/java/blue/golem/android/walletthing/MainActivity.java
@@ -2,12 +2,30 @@ package blue.golem.android.walletthing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    CurrencyConverter currencyConverter = new CurrencyConverter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Spinner fromSpinner = findViewById(R.id.fromSpinner);
+        Spinner toSpinner = findViewById(R.id.toSpinner);
+        Set<String> currencies = currencyConverter.getCurrencies();
+        String[] currArr = currencies.toArray(new String[0]);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, currArr);
+        fromSpinner.setAdapter(adapter);
+        toSpinner.setAdapter(adapter);
     }
 }
