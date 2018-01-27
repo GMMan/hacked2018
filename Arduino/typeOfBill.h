6b13d4806d71e6379ba501cf87d6ed3ca6d4d690
fdrury <fdrury@ualberta.ca>
2018-01-27 15:04:34 -0700

    fixed header prototypes

---
+++ Arduino/typeOfBill.h
@@ -0,0 +1,8 @@
+#ifdef TYPEOFBILL_H
+#define TYPEOFBILL_H
+
+// returns index of matching bill
+unsigned int typeOfBill(int numColors, int numReadings, int databaseSize,
+        int reading[numReadings][numColors], int database[databaseSize][numReadings][numColors]);
+
+#endif
